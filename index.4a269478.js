1ab61798ea8e4c4056740016888b0c339fbfdcab
igor-kulish <106305335+igor-kulish@users.noreply.github.com>
2022-08-18 10:35:02 +0000

    Deploying to gh-pages from @ igor-kulish/HTML-CSS-FINAL-PROJECT@d9e82ed10186bc06b66ffe02c25a914112fc20dc 🚀

---
+++ index.4a269478.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[js-data-modal-open]"),closeModalBtn:document.querySelector("[js-data-modal-close]"),modal:document.querySelector("[js-data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.4a269478.js.map
---
+++ index.caadccc4.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[js-data-modal-open]"),closeModalBtn:document.querySelector("[js-data-modal-close]"),modal:document.querySelector("[js-data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.caadccc4.js.map
